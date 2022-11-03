2b707b51262cd5d219c5d86812082971f1e70f66
diego-654 <diegovalerio654@gmail.com>
2022-11-03 16:39:03 -0500

    migracion Especificacion

---
+++ database/migrations/2022_11_03_211700_create_especificacions_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {Schema::create('especificacions', function (Blueprint $table) {
+        $table->id('ID_Especificacion');
+        $table->string('Procesador_Esp');
+        $table->string('MemoriaRAM_Esp');
+        $table->string('DiscDuro_Esp');
+        $table->string('TVideo_Esp');
+        $table->string('SistemaOp_Esp');
+        $table->string('VelocidadProc_Esp');
+        $table->string('TamañoPantalla_Esp');
+        $table->string('ResolucionPantalla_Esp');
+        $table->string('CamaraWeb_Microfono_Esp');
+        $table->integer('LectorTMemoria_Esp');
+        $table->integer('PuertoRJ45_Esp');
+        $table->integer('PuertoHDMI_Esp');
+        $table->integer('PuertoUSB_Esp');
+        $table->integer('Wifi_Esp');
+        $table->integer('ConectBluetooth_Esp');
+        $table->integer('PuertoC_Esp');
+        $table->integer('SuiteInformatica_Esp');
+        $table->integer('TecladoMouse_Esp');
+        $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('especificacions');
+    }
+};
