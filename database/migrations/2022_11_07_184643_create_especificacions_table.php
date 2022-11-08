176a2c2925a30ced7b140dc0331695aad1d47504
eddievasquez2110 <eddieomarvasquez@gmail.com>
2022-11-08 12:45:07 -0500

    update last migration

--- database/migrations/2022_11_07_184643_create_especificacions_table.php
+++ database/migrations/2022_11_07_184643_create_especificacions_table.php
@@ -15,23 +15,105 @@ return new class extends Migration
     {
         Schema::create('especificacions', function (Blueprint $table) {
             $table->id('ID_Especificacion');
-            $table->unsignedBigInteger('ID_Esp_Monitor')->nullable();
-            $table->foreign('ID_Esp_Monitor')->references('ID_Esp_Monitor')->on('esp__monitors');
-            $table->unsignedBigInteger('ID_Esp_Impresora_BlancoNegro')->nullable();
-            $table->foreign('ID_Esp_Impresora_BlancoNegro')->references('ID_Esp_Impresora_BlancoNegro')->on('esp__impresora__blanco_negros');
-            $table->unsignedBigInteger('ID_Esp_Impresora_Color')->nullable();
-            $table->foreign('ID_Esp_Impresora_Color')->references('ID_Esp_Impresora_Color')->on('esp__impresora__colors');
-            $table->unsignedBigInteger('ID_Esp_Proyector')->nullable();
-            $table->foreign('ID_Esp_Proyector')->references('ID_Esp_Proyector')->on('esp__proyectors');
-            $table->unsignedBigInteger('ID_PantallaInteractiva')->nullable();
-            $table->foreign('ID_PantallaInteractiva')->references('ID_PantallaInteractiva')->on('esp__pantalla_interactivas');
-            $table->unsignedBigInteger('ID_Esp_DiscoDuro')->nullable();
-            $table->foreign('ID_Esp_DiscoDuro')->references('ID_Esp_DiscoDuro')->on('esp__disco_duros');
-            $table->unsignedBigInteger('ID_Esp_Tablet')->nullable();
-            $table->foreign('ID_Esp_Tablet')->references('ID_Esp_Tablet')->on('esp__tablets');
-            $table->unsignedBigInteger('ID_Esp_Ordenador')->nullable();
-            $table->foreign('ID_Esp_Ordenador')->references('ID_Esp_Ordenador')->on('esp__ordenadors');
-            $table->string('Estado_Especificacion')->nullable();
+            $table->foreign('ID_Uso_Equipo')->references('ID_Uso_Equipo')->on('uso__equipos');
+            $table->unsignedBigInteger('ID_Uso_Equipo');
+            $table->string('Ord_Procesador',45)->nullable();
+            $table->string('Ord_MemoriaRAM',45)->nullable();
+            $table->string('Ord_DiscDuro',45)->nullable();
+            $table->string('Ord_TVideo',45)->nullable();
+            $table->string('Ord_SistemaOp',45)->nullable();
+            $table->string('Ord_VelocidadProc',45)->nullable();
+            $table->string('Ord_TamañoPantalla',45)->nullable();
+            $table->string('Ord_CamaraWebMicrofono',45)->nullable();
+            $table->tinyInteger('Ord_LectorTMemoria')->nullable();
+            $table->tinyInteger('ord_PuertoRJ45')->nullable();
+            $table->tinyInteger('Ord_PuertoHDMI')->nullable();
+            $table->tinyInteger('Ord_PuertoUSB')->nullable();
+            $table->tinyInteger('Ord_Wifi')->nullable();
+            $table->tinyInteger('Ord_ConectBluetooth')->nullable();
+            $table->tinyInteger('Ord_PuertoTipoC')->nullable();
+            $table->tinyInteger('Ord_SuiteInformatica')->nullable();
+            //ADD CPU
+            $table->tinyInteger('Ord_PuertoHDMIyVGA_PuertoHDMIyDP')->nullable();
+            $table->String('Ord_ConectBluetoothOpc',10)->nullable();
+            $table->tinyInteger('Ord_TecladoMouse')->nullable();
+
+            //monitor
+            $table->string('Mon_TamañoPantalla',45)->nullable();
+            $table->string('Mon_CamaraWebPantalla',45)->nullable();
+            $table->string('Mon_MicrofonoPantalla',45)->nullable();
+            $table->string('Mon_TipoPantalla',45)->nullable();
+            $table->string('Mon_ResolucionPantalla',45)->nullable();
+            $table->tinyInteger('Mon_PuertoHDMI')->nullable();
+            $table->String('Mon_PuertoVGA',10)->nullable();
+            $table->String('Mon_DisplayPort')->nullable();
+
+            //impresora blanco y negro
+            $table->string('ImpBn_Funciones')->nullable();
+            $table->string('ImpBn_VelocidadImpresion')->nullable();
+            $table->string('ImpBn_ImpBnTecnologiaImpresion')->nullable();
+            $table->string('ImpBn_CalidadImpresion')->nullable();
+            $table->string('ImpBn_LenguajeImpresion')->nullable();
+            $table->string('ImpBn_DoblecaraImpre')->nullable();
+            $table->string('ImpBn_Conectividad')->nullable();
+            $table->string('ImpBn_MemoriaStandard')->nullable();
+            $table->string('ImpBn_TamaniosSoporte')->nullable();
+            $table->string('ImpBn_TipoEscaner')->nullable();
+
+            //impresora a color
+            $table->string('ImpColor_TecnologiaImpresion')->nullable();
+            $table->string('ImpColor_InyeccionTinta')->nullable();
+            $table->string('ImpColor_ResolucionMaxImpresion')->nullable();
+            $table->string('ImpColor_Velocidad_Impresion')->nullable();
+            $table->string('ImpColor_Copias')->nullable();
+            $table->string('ImpColor_Escaneo')->nullable();
+            $table->string('ImpColor_ConectividadTipo')->nullable();
+            $table->string('ImpColor_SoportePapelSinPC')->nullable();
+            $table->string('ImpColor_CapacidadCargaPapel')->nullable();
+
+            //proyector
+            $table->string('Proyec_Resolucion')->nullable();
+            $table->string('Proyec_Tipo')->nullable();
+            $table->string('Proyec_Brillo')->nullable();
+            $table->string('Proyec_Conectividad')->nullable();
+            $table->string('Proyec_RelacionAspecto')->nullable();
+            $table->string('Proyec_PuertoHDMI')->nullable();
+            $table->string('Proyec_PuertoVGA')->nullable();
+            $table->string('Proyec_EntradaRJ45')->nullable();
+            $table->string('Proyec_VidaUtilLampara')->nullable();
+
+            //pantalla interactiva
+            $table->string('PantInter_TamañoDiagonal')->nullable();
+            $table->string('PantInter_Tipo')->nullable();
+            $table->string('PantInter_AnguloVista')->nullable();
+            $table->string('PantInter_Resolucion')->nullable();
+            $table->string('PantInter_RatioContraste')->nullable();
+            $table->string('PantInter_RelacionContraste')->nullable();
+            $table->integer('PantInter_EntradaHDMI_VGA')->nullable();
+            $table->Integer('PantInter_EntradaUSB')->nullable();
+            $table->tinyInteger('PantInter_SalidaAudio')->nullable();
+            $table->tinyInteger('PantInter_EntradaRS232')->nullable();
+            $table->tinyInteger('PantInter_EntradaRJ45')->nullable();
+            $table->tinyInteger('PantInter_Wifi')->nullable();
+            $table->tinyInteger('PantInter_Bluetooth')->nullable();
+
+            //disco duro
+            $table->string('DiscDuro_CapacidadAlmacenamiento')->nullable();
+            $table->string('DiscDuro_Tipo')->nullable();
+            $table->string('DiscDuro_InterfaceConexion')->nullable();
+            $table->string('DiscDuro_FuenteAlimentacion')->nullable();
+            $table->string('DiscDuro_PlataformaTrabajo')->nullable();
+
+            //tablets
+            $table->string('Tablet_Pantalla')->nullable();
+            $table->string('Tablet_Procesador')->nullable();
+            $table->string('Tablet_MemoriaRAM')->nullable();
+            $table->string('Tablet_MemoriaInterna')->nullable();
+            $table->tinyInteger('Tablet_Bluetooth')->nullable();
+            $table->string('Tablet_CamaraPrincipal')->nullable();
+            $table->string('Tablet_CamaraFrontal')->nullable();
+
+            $table->string('Estado_Especificacion',20);
             $table->timestamps();
         });
     }
