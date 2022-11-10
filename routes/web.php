6b38fc99713d79928e9e4248b32d438b8e1f8b68
diego-654 <diegovalerio654@gmail.com>
2022-11-10 12:11:31 -0500

    migracion de versiones

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,52 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard'); 
 
 require __DIR__.'/auth.php';
+
+
+//Ejemplo1
+Route::get('/texto',function(){
+    return '<h1>Esto es un texto de prueba</h1>';
+    
+});
+
+//Ejemplo2 - con array
+Route::get('/arreglo',function(){
+    $arreglo=[
+        'ID'=>'1',
+        'Descripcion'=>'Producto 1'
+    ];
+    return $arreglo;
+});
+//Ejemplo 3 - con parametros
+//Ejemplo1
+Route::get('/nombre/{nombre}',function($nombre){
+    return '<h1>El nombre es: '.$nombre.'</h1>';
+    
+});
+
+Route::get('/cliente/{cliente?}',function($cliente='Cliente General'){
+    return '<h1>El cliente es: '.$cliente.'</h1>';
+    
+});
+
+//Ejemplo 5
+Route::get('/ruta1',function(){
+    return '<h1>Esta es la vista de la ruta 1</h1>';
+
+})->name('Ruta1');
+
+Route::get('/ruta2',function(){
+    //return '<h1>Esta es la vista de la ruta 2</h1>';
+    return redirect()->route('Ruta1');
+});
+
+
+//ejemplo 6
+Route::get('/usuario/{usuario}',function($usuario){
+    return '<h1>El usuario es: '.$usuario.'</h1>';
+    
+})->where('usuario','[0-9]+');
+//where('usuario','[A-Za-z]+');
