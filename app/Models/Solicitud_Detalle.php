34ba27b974c6a92d74672baee422a4142849d12e
eddievasquez2110 <eddieomarvasquez@gmail.com>
2022-11-10 00:30:50 -0500

    Correcion de migraciones y modelos

--- app/Models/Solicitud_Detalle.php
+++ app/Models/Solicitud_Detalle.php
@@ -10,7 +10,9 @@ class Solicitud_Detalle extends Model
     use HasFactory;
 
     protected $fillable = [
+        'ID_Software',
+        'ID_Equipo',
+        'ID_Software',
         'Descripcion_SolicitudDetalle',
     ];
-
 }
