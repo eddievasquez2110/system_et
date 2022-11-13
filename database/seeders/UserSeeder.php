fe3eb7601090191ac77025852d8896ceaf2de473
eddievasquez2110 <eddieomarvasquez@gmail.com>
2022-11-13 10:59:09 -0500

    update seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -15,9 +15,25 @@ class UserSeeder extends Seeder
         $user = User::create([
             'ID_Rol'=>1,
             'ID_Oficina'=>1,
+            'name'=>'Superadmin',
+            'email'=>'s_admin@gmail.com',
+            'password'=>Hash::make('s_admin'),
+        ]);
+
+        $user = User::create([
+            'ID_Rol'=>2,
+            'ID_Oficina'=>1,
             'name'=>'admin',
             'email'=>'admin@gmail.com',
-            'password'=>'admin',
+            'password'=>Hash::make('admin'),
+        ]);
+
+        $user = User::create([
+            'ID_Rol'=>3,
+            'ID_Oficina'=>2,
+            'name'=>'user1',
+            'email'=>'user1@gmail.com',
+            'password'=>Hash::make('user1'),
         ]);
     }
 }
