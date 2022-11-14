4de5c8911eef7fa1c02adc418cd12ecbac258bc6
diego-654 <diegovalerio654@gmail.com>
2022-11-14 10:43:22 -0500

    nada

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -6,8 +6,9 @@ import InputLabel from '@/Components/InputLabel';
 export default function Guest({ children }) {
     return (
         <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+            
             <div className='w-full sm:max-w-md px-6 py-4 bg-green-900 overflow-hidden text-center '>
-                
+            <ApplicationLogo></ApplicationLogo>            
             </div>
             <div className="w-full sm:max-w-md px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {children}
