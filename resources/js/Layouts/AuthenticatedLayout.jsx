68b79fa87c35c8070b061d528adddfccb4251004
diego-654 <diegovalerio654@gmail.com>
2022-11-17 09:49:41 -0500

    .

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -4,11 +4,14 @@ import Dropdown from '@/Components/Dropdown';
 import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import { Link } from '@inertiajs/inertia-react';
+import PrimaryButton from '@/Components/PrimaryButton';
+
 
 export default function Authenticated({ auth, header, children }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
 
     return (
+        
         <div className="min-h-screen bg-gray-100">
             <nav className="bg-green-900 border-b border-gray-100">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -19,6 +22,7 @@ export default function Authenticated({ auth, header, children }) {
                                     <ApplicationLogo className="block text-gray-500"/>
                                 </Link>
                             </div>
+                            
 
                             <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                                 <NavLink href={route('dashboard')} active={route().current('dashboard')}>
@@ -71,7 +75,8 @@ export default function Authenticated({ auth, header, children }) {
                                     </Dropdown.Content>
                                 </Dropdown>
                             </div>
-                        </div>                        
+                        </div> 
+                                               
 
                         <div className="-mr-2 flex items-center sm:hidden">
                             <button
@@ -127,6 +132,7 @@ export default function Authenticated({ auth, header, children }) {
                             </ResponsiveNavLink>
                         </div>
                     </div>
+                    
                 </div>
             </nav>
 
@@ -137,6 +143,7 @@ export default function Authenticated({ auth, header, children }) {
             )}
 
             <main>{children}</main>
+            
         </div>
     );
 }
--- resources/js/Pages/Admin.jsx
+++ resources/js/Pages/Admin.jsx
@@ -18,11 +18,11 @@ export default function Admin(props) {
         <Head title="Dashboard" />
             <div className='grid grid-cols-4 divide-x'>
                 <div className='w-4/5 h-screen bg-green-400 px-10 py-10 space-y-5'>
-                <PrimaryButton>Gestión de Usuarios</PrimaryButton>
-                <PrimaryButton>Gestión de Solicitudes</PrimaryButton>
-                <PrimaryButton>Gestión de Software</PrimaryButton>
-                <PrimaryButton>Gestión de Equipos</PrimaryButton>
-                <PrimaryButton>Reporte</PrimaryButton>
+                    <PrimaryButton>Gestión de Usuarios</PrimaryButton>
+                    <PrimaryButton>Gestión de Solicitudes</PrimaryButton>
+                    <PrimaryButton>Gestión de Software</PrimaryButton>
+                    <PrimaryButton>Gestión de Equipos</PrimaryButton>
+                    <PrimaryButton>Reporte</PrimaryButton>
                 </div>
                 
                 <div>
