008a14fa1627f0970a7b1ead65bc3bdc73c7278b
JhonatanRaul16 <jhonatanraul16@gmail.com>
2022-11-17 10:56:28 -0500

    xd

--- app/Models/Tipo_Equipo.php
+++ app/Models/Tipo_Equipo.php
@@ -8,10 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Tipo_Equipo extends Model
 {
     use HasFactory;
-<<<<<<< HEAD
-    protected $table = "Tipo__Equipos";
-    public function Equipo() {
-=======
 
     protected $fillable = [
         'Nombre_Tipo_Equipo',
@@ -19,7 +15,6 @@ class Tipo_Equipo extends Model
 
     public function Equipo()
     {
->>>>>>> 688564f22e811ba4aae8cf827a76e39917cf3332
         return $this->belongsTo(Equipo::class);
     }
 }
