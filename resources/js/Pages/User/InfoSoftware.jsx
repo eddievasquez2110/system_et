267dcd27bff7d1297dfa29a60fa17d9a8bfc42cf
eddievasquez2110 <eddieomarvasquez@gmail.com>
2022-12-06 22:01:18 -0500

    software_update_2

--- resources/js/Pages/User/InfoSoftware.jsx
+++ resources/js/Pages/User/InfoSoftware.jsx
@@ -4,19 +4,15 @@ import { Head } from '@inertiajs/inertia-react';
 import InfoSoftwares from '@/Components/InfoSoftwares';
 import NavbarSAdmin from '@/Layouts/NavBarSAdmi';
 
-const InfoSoftware = ({auth, infoSoftwares, usuarios, oficina}) => {
-    // console.log(auth)
-    // console.log(auth.user.id)
-    console.log(infoSoftwares)
-    console.log(usuarios)
-    console.log(oficina)
+const InfoSoftware = ({auth, infoSoftwares}) => {
+    
     return (
         <>
         {auth.user.ID_Rol == 1 ?
             
             <NavbarSAdmin auth={auth}>
-                <div className='w-full h-20 backdrop-blur-sm bg-white/30 grid place-items-center'>SOFTWARES</div>
-                <Head title="Especificaciones" />
+            <Head title="Softwares" />
+                <div className='mt-5 w-full h-20 backdrop-blur-sm bg-white/30 grid place-items-center'>SOFTWARES</div>
                     <div className="wrapper flex flex-wrap items-center justify-center gap-5 mt-8 p-10">
                         {
                             infoSoftwares.map(infoSoftware =>
@@ -27,8 +23,8 @@ const InfoSoftware = ({auth, infoSoftwares, usuarios, oficina}) => {
             </NavbarSAdmin>
             : auth.user.ID_Rol == 3 ?
                 <AuthenticatedLayout auth={auth}>
-                <div className='w-full h-20 backdrop-blur-sm bg-white/30 grid place-items-center'>SOFTWARES</div>
-                <Head title="Especificaciones" />
+                <Head title="Softwares" />
+                <div className='mt-5 w-full h-20 backdrop-blur-sm bg-white/30 grid place-items-center'>SOFTWARES</div>
                     <div className="wrapper flex flex-wrap items-center justify-center gap-5 mt-8">
                         {
                             infoSoftwares.map(infoSoftware =>
